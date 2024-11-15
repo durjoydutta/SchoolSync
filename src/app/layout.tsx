2bc8918d86cab79e9386e70625746896010d6e86
durjoydutta001 <durjoydutta001@gmail.com>
2024-11-15 16:53:55 +0000

    minor naming changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
+  title: "SchoolSync - A Parent Teacher Interaction Management System",
   description: "Next.js School Management System",
 };
 
