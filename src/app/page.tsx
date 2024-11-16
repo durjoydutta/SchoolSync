2b7206be76f6424d57fa9d83db93774974db3199
durjoydutta001 <durjoydutta001@gmail.com>
2024-11-16 09:06:22 +0000

    added some minor css to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,16 @@ import Link from "next/link";
 
 const Homepage = () => {
   return (
-    <>
-    <div className=''>Homepage</div>
-    <Link href="/admin">AdminPanel</Link>
-    </>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100"> {/* Added Tailwind classes */}
+      <div className="text-3xl font-bold text-gray-800">Homepage</div> {/* Enhanced styling */}
+      <Link 
+        href="/admin" 
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" // Added Tailwind classes
+      >
+        Admin Panel
+      </Link>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
