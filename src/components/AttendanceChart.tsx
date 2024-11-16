f448aa5b493686fb650d0282e8f160fa92097e67
durjoydutta001 <durjoydutta001@gmail.com>
2024-11-16 12:04:43 +0000

    modified the ui of attendance chart

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -1,58 +1,74 @@
 "use client";
 import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import Image from 'next/image';
 
 const data = [
     {
-      name: "Mon",
-      present: 60,
-      absent: 40,
+        name: "Mon",
+        present: 69,
+        absent: 31,
     },
     {
-      name: "Tue",
-      present: 70,
-      absent: 60,
+        name: "Tue",
+        present: 70,
+        absent: 60,
     },
     {
-      name: "Wed",
-      present: 90,
-      absent: 75,
+        name: "Wed",
+        present: 90,
+        absent: 75,
     },
     {
-      name: "Thu",
-      present: 90,
-      absent: 75,
+        name: "Thu",
+        present: 90,
+        absent: 75,
     },
     {
-      name: "Fri",
-      present: 65,
-      absent: 55,
+        name: "Fri",
+        present: 31,
+        absent: 69,
     },
-  ];
+];
 
 const AttendanceChart = () => {
     return (
-        <div className="bg-white rounded-xl w-full h-full p-4">
-      <ResponsiveContainer width="100%" height="100%">
-        <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="present" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
-          <Bar dataKey="absent" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
-        </BarChart>
-      </ResponsiveContainer>
+        <div className="bg-white rounded-xl w-full h-full p-4 mb-3">
+            <div className="flex justify-between items-center">
+                <h1 className="text-lg font-semibold">Attendance</h1>
+                <Image src="/moreDark.png" alt="" width={20} height={20} />
+            </div>
+            <ResponsiveContainer width="100%" height="90%">
+                <BarChart width={500} height={300} data={data} barSize={16}>
+                    <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#ddd" />
+                    <XAxis
+                        dataKey="name"
+                        axisLine={false}
+                        tick={{ fill: "#d1d5db" }}
+                        tickLine={false}
+                    />
+                    <YAxis axisLine={false} tick={{ fill: "#d1d5db" }} tickLine={false} />
+                    <Tooltip
+                        contentStyle={{ borderRadius: "10px", borderColor: "lightgray" }}
+                    />
+                    <Legend
+                        align="left"
+                        verticalAlign="top"
+                        wrapperStyle={{ paddingTop: "20px", paddingBottom: "40px" }}
+                    />
+                    <Bar
+                        dataKey="present"
+                        fill="#53F0BC"
+                        legendType="circle"
+                        radius={[10, 10, 0, 0]}
+                    />
+                    <Bar
+                        dataKey="absent"
+                        fill="#F25E7A"
+                        legendType="circle"
+                        radius={[10, 10, 0, 0]}
+                    />
+                </BarChart>
+            </ResponsiveContainer>
         </div>
     )
 }
