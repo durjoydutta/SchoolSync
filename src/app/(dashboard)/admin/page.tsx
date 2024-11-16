67a57dd457170755993661ea8103ea3e6fca9d74
durjoydutta001 <durjoydutta001@gmail.com>
2024-11-16 07:50:40 +0000

    added sex ratio chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,6 @@
 // import Announcements from "@/components/Announcements";
 // import AttendanceChart from "@/components/AttendanceChart";
-// import CountChart from "@/components/CountChart";
+import RatioChart from "@/components/RatioChart";
 // import EventCalendar from "@/components/EventCalendar";
 // import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
@@ -17,14 +17,14 @@ const AdminPage = () => {
           <UserCard type="teacher" />
           <UserCard type="staff" />
         </div>
-        {/* MIDDLE CHARTS */}
+        {/* SEX-RATIO CHARTS */}
         <div className="flex gap-4 flex-col lg:flex-row">
           {/* COUNT CHART */}
-          <div className="w-full lg:w-1/3 h-[450px]">
-            {/* <CountChart /> */}
+          <div className="w-full h-[450px]">
+            <RatioChart />
           </div>
           {/* ATTENDANCE CHART */}
-          <div className="w-full lg:w-2/3 h-[450px]">
+          <div className="w-full h-[450px]">
             {/* <AttendanceChart /> */}
           </div>
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,4 @@
-import Link from "next/link";
-import Image from "next/image";
-
+import Image from "next/image"
 
 const Navbar = () => {
   return (
@@ -19,8 +17,8 @@ const Navbar = () => {
           <Image src="/announcement.png" alt="" width={20} height={20}/>
           <div className='absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full text-xs'>1</div>
         </div>
-        <div className='flex flex-col items- truncate ...'>
-          <span className="text-xs leading-3 font-medium">DDC</span>
+        <div className='flex flex-col'>
+          <span className="text-xs leading-3 font-medium">John Doe</span>
           <span className="text-[10px] text-gray-500 text-right">Admin</span>
         </div>
         <Image src="/avatar.png" alt="" width={36} height={36} className="rounded-full"/>
