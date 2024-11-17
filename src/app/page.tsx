7ca92f86da6a914e9bd8025b2af435b5207a671e
durjoydutta001 <durjoydutta001@gmail.com>
2024-11-17 19:40:00 +0000

    changed homepage bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 
 const Homepage = () => {
   return (
-    <div className="min-h-screen w-full flex flex-col items-center justify-center bg-lamaPurple dark:bg-#121212 gap-4">
-      <div className="text-3xl font-bold text-gray-800">SchoolSync</div>
-      <div className="text-xl font-bold text-gray-800">Next-Gen School Management</div>
-      <div className="panels flex justify-between gap-2">
+    <div className="min-h-screen w-full flex flex-col items-center justify-center bg-smoke-900 dark:bg-#121212 gap-4">
+      <div className="text-4xl font-bold text-gray-800">SchoolSync</div>
+      <div className="flex flex-col justify-center items-center gap-2 text-xl font-bold text-gray-800">
+        <span>Welcome to</span>
+        <span>Next-Gen School Management</span>
+      </div>
+      <div className="panels flex justify-between flex-wrap gap-2">
         <Link
           href="/admin"
           className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
