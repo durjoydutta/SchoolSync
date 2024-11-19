80250d9ceb2c0fd4588af81f4742ab7f214ba673
durjoydutta001 <durjoydutta001@gmail.com>
2024-11-19 13:04:10 +0000

    update navbar responsiveness to match sidebar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,10 +25,10 @@ const Navbar = () => {
     <div className="flex items-center justify-between w-full gap-2 px-4 py-4">
       {/* LEFT */}
       <div className="flex justify-start items-center gap-2">
-        <div className="flex gap-2 items-center md:hidden">
+        <div className="flex gap-2 items-center">
           <SchoolSyncLogo />
         </div>
-        <Separator orientation="vertical" className="hidden sm:block md:hidden h-6" />
+        <Separator orientation="vertical" className="hidden sm:block h-6" />
         <Breadcrumb>
           <BreadcrumbList className="hidden sm:flex flex-nowrap justify-between mt-1">
             <BreadcrumbItem>
@@ -44,21 +44,21 @@ const Navbar = () => {
         </Breadcrumb>
       </div>
       {/* RIGHT */}
-      <div className="w-full flex items-center justify-end gap-4 md:gap-6 py-4">
+      <div className="w-full flex items-center justify-end gap-4 md:gap-6 py-4 mt-1">
         <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
-          <Image src="/message.png" alt="Messages" width={20} height={20} />
+          <Image src="/message.png" alt="Messages" width={16} height={16} />
         </div>
         <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative">
-          <Image src="/announcement.png" alt="Announcements" width={20} height={20} />
+          <Image src="/announcement.png" alt="Announcements" width={18} height={18} />
           <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full text-xs">
             1
           </div>
         </div>
-        <div className="flex flex-col items-end">
+        <div className="flex flex-col items-end md:hidden">
           <span className="text-xs leading-3 font-medium">DDC</span>
           <span className="text-[10px] text-gray-500 text-right">Admin</span>
         </div>
-        <Avatar>
+        <Avatar className="md:hidden">
           <AvatarImage src="https://github.com/durjoydutta.png" alt="User avatar" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
