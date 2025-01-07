bd209e11ba62a0a5d9c44178ade28dba81eb0e41
Durjoy <contact.durjoy@gmail.com>
2025-01-07 18:46:25 +0530

    ignore eslint check during build

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,12 @@
 const nextConfig = {
     images:{
         remotePatterns: [{hostname: "images.pexels.com"}],
-    }
-
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
