a2257e302c60be634b574ff6f4fe0849a63293bb
Durjoy <contact.durjoy@gmail.com>
2025-01-07 15:17:57 +0530

    fetching list of parents from db

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -1,10 +1,11 @@
 import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
-import TableSearch from '@/components/TableSearch'
-import { parentsData, role } from '@/lib/data';
+import TableSearch from '@/components/TableSearch';
+import { role } from '@/lib/data';
 import Image from 'next/image';
-import React from 'react'
+import React from 'react';
 import Link from "next/link";
+import prisma from '@/lib/prisma';
 
 type Parent = {
   id: number;
@@ -14,6 +15,7 @@ type Parent = {
   phone: string;
   address: string;
 };
+
 const columns = [
   {
     header: "Info",
@@ -40,8 +42,19 @@ const columns = [
   }
 ];
 
+const ParentList = async ({ searchParams }: { searchParams: { [key: string]: string | undefined } }) => {
+  const { page } = searchParams;
+  const p = page ? parseInt(page) : 1;
 
-const ParentList = () => {
+  const parents = await prisma.parent.findMany({
+    include: {
+      students: true, // Include students data if needed
+    },
+    take: 10,
+    skip: (p - 1) * 10,
+  });
+
+  const count = await prisma.parent.count();
 
   const renderRow = (item: Parent) => (
     <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm
@@ -54,12 +67,14 @@ const ParentList = () => {
           <p className="text-xs text-gray-500">{item?.email}</p>
         </div>
       </td>
-      <td className="hidden md:table-cell">{item.students.join(",")}</td>
+      <td className="hidden md:table-cell">
+        {item.students.map((student) => student.name).join(",")}
+      </td>
       <td className="hidden md:table-cell">{item.phone}</td>
       <td className="hidden md:table-cell">{item.address}</td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
+          <Link href={`/list/parents/${item.id}`}>
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky dark:bg-sky-300">
               <Image src="/assets/edit.png" alt="" width={16} height={16} className='dark:bg-sky-300' />
             </button>
@@ -72,16 +87,14 @@ const ParentList = () => {
         </div>
       </td>
     </tr>
-  )
-
-
+  );
 
   return (
     <div className='bg-white dark:bg-stone-800 p-4 rounded-md flex-1'>
       {/* TOP */}
       <div className='flex items-center justify-between'>
         <h1 className='hidden md:block text-lg font-semibold'>All Parents</h1>
-        <div className='flex flex-col md:flex-row items-cener gap-4 w-full md:w-auto'>
+        <div className='flex flex-col md:flex-row items-center gap-4 w-full md:w-auto'>
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
             <button className="w-8 h-8 flex items-center justify-center rounded-full dark:bg-lamaYellowLight bg-lamaYellow" >
@@ -98,13 +111,12 @@ const ParentList = () => {
       </div>
       {/* LIST */}
       <div className=''>
-        <Table columns={columns} renderRow={renderRow} data={parentsData} />
+        <Table columns={columns} renderRow={renderRow} data={parents} />
       </div>
       {/* PAGINATION */}
-      <Pagination />
-
+      <Pagination page={p} count={count} />
     </div>
-  )
+  );
 }
 
-export default ParentList
+export default ParentList;
